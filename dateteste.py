0ebf7f3c1a834ed23df01b8976af5b1e96772bbc
Bruno Mendes <bmendesl@4linux.com>
2018-11-05 19:24:32 -0200

    update datateste

--- dateteste.py
+++ dateteste.py
@@ -1,31 +1,11 @@
 #!/usr/bin/python3
 
-# from datetime import timedelta
-# from datetime import date
-# from datetime import datetime
+#https://pypi.org/project/isoweek/
 
-# #print(date(2018, 11, 1).weekday())
-# print(date(2018, 11, 1).isocalendar())
+from isoweek import Week
 
-from datetime import datetime, date, time
-# Using datetime.combine()
-d = date(2005, 7, 14)
-t = time(12, 30)
-
-dt = datetime.today()
-dt
-
-ic = dt.isocalendar()
-for it in ic:   
-   print(it)
-
-# 2006    # ISO year
-# 47      # ISO week
-# 2       # ISO weekday
-# Formatting datetime
-dt.strftime("%A, %d. %B %Y %I:%M%p")
-'Tuesday, 21. November 2006 04:30PM'
-'The {1} is {0:%d}, the {2} is {0:%B}, the {3} is {0:%I:%M%p}.'.format(dt, "day", "month", "time")
-'The day is 21, the month is November, the time is 04:30PM.'
-
-https://pypi.org/project/isoweek/
+w = Week(2018, 45)
+#print("Week %s starts on %s" % (w, w.monday()))
+print("Week %s starts on %s" % (w, w.monday().strftime('%Y%m%d')))
+#print("Current week number is", Week.thisweek().week)
+#print("Next week is", Week.thisweek() + 1)
