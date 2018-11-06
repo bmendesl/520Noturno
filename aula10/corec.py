2fb5371890f2b7f0342811fdf7b0a76c2f897276
Bruno Mendes <bmendesl@4linux.com>
2018-11-05 22:04:26 -0200

    chat

---
+++ aula10/corec.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import paramiko
+try:
+    client = paramiko.client.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(
+        '192.168.202.1',
+        username='sabado',
+        password='sabado'
+    )
+except Exception as e:
+    print('Erro: {}'.format(e))
+    exit()
+
+stdin, stdout, stderr = client.exec_command('hostname')
+
+if stdout.channel.recv_exit_status() == 0:
+    print(stdout.read().decode('utf-8'))
+else:
+    print(stderr.read().decode('utf-8'))
+
+# client.connect(
+#     '192.168.202.1',
+#     username='sabado',
+#     password='sabado',
+#     port='22'
+# )
---
+++ modulos/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import banco as banco
+import threading
+
+if __name__ == '__main__':
+    user = input('Nickname: ')
+
+    f = threading.Thread(target=banco.visualizar)
+    f.start()
+
+    while f.isAlive:
+        mens = input()
+        banco.cadastrar(user,mens)
